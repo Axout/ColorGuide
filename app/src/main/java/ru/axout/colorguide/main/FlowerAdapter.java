eb17def4f06caacbb81521b1adaf6d6eec0e0041
Sergey Rakov <rako-serge@yandex.ru>
2021-08-01 00:27:16 +0300

    add View

---
+++ app/src/main/java/ru/axout/colorguide/main/FlowerAdapter.java
@@ -0,0 +1,79 @@
+package ru.axout.colorguide.main;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.squareup.picasso.Picasso;
+import ru.axout.colorguide.R;
+import ru.axout.colorguide.model.Flower;
+
+import java.util.List;
+
+public class FlowerAdapter extends RecyclerView.Adapter<FlowerAdapter.FlowerHolder> {
+
+    private Context context;
+    private List<Flower> flowers;
+
+    public FlowerAdapter(Context context, List<Flower> flowers) {
+        this.context = context;
+        this.flowers = flowers;
+    }
+
+    public void setFlowers(List<Flower> flowers) {
+        this.flowers = flowers;
+        // Сообщим всем зарегистрированным наблюдателям, что данные изменились.
+        // Это необходимо для обновления отображения RecyclerView.
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public FlowerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.flower_item, parent, false);
+        return new FlowerHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FlowerHolder holder, int position) {
+        Flower item = flowers.get(position);
+        holder.flower = item;
+
+        holder.flowerRus.setText(item.getFlower_rus());
+        holder.flowerLat.setText(item.getFlower_lat());
+        holder.flowerDesc.setText(item.getFlower_desc());
+
+        Picasso.get()
+                .load(item.getFlower_img())
+                .error(R.drawable.ic_florist)
+                .placeholder(R.drawable.ic_florist)
+                .fit()
+                .into(holder.flowerImg);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (flowers == null) return 0;
+        else return flowers.size();
+    }
+
+    static class FlowerHolder extends RecyclerView.ViewHolder {
+
+        Flower flower;
+        ImageView flowerImg;
+        TextView flowerRus, flowerLat, flowerDesc;
+
+        FlowerHolder(@NonNull View itemView) {
+            super(itemView);
+
+            flowerImg = itemView.findViewById(R.id.flower_img);
+            flowerRus = itemView.findViewById(R.id.flower_rus);
+            flowerLat = itemView.findViewById(R.id.flower_lat);
+            flowerDesc = itemView.findViewById(R.id.flower_desc);
+        }
+    }
+}
--- app/src/main/java/ru/axout/colorguide/main/MainActivity.java
+++ app/src/main/java/ru/axout/colorguide/main/MainActivity.java
@@ -1,18 +1,44 @@
 package ru.axout.colorguide.main;
 
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import ru.axout.colorguide.R;
+import ru.axout.colorguide.model.Flower;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainContract.MainView{
 
     RecyclerView recyclerView;
+    FlowerAdapter flowerAdapter;
+    MainContract.MainPresenter presenter;
+    ArrayList<Flower> flowerArrayList = new ArrayList<Flower>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.rv_flowers);
+
+        flowerAdapter = new FlowerAdapter(this, flowerArrayList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(flowerAdapter);
+
+        presenter = new MainPresenter(this);
+        presenter.getAllFlowers();
+    }
+
+    @Override
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showAllFlowers(List<Flower> flowers) {
+        flowerAdapter.setFlowers(flowers);
     }
 }
